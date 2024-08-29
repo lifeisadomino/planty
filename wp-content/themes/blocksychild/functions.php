60ecc09cc0c535b8b90c9a5e0de7a6c49b93fa72
Audrey Bisgambiglia <lifeisadomino@hotmail.com>
2024-08-29 12:07:31 +0200

    finalisation projet 6

--- wp-content/themes/blocksychild/functions.php
+++ wp-content/themes/blocksychild/functions.php
@@ -6,3 +6,24 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
+
+function lien_admin($items) {
+    if (is_user_logged_in()) {
+        // Création du lien Admin
+        $lien_admin = '<li class="lienAdmin"><a href="'. get_admin_url() .'">Admin</a></li>';
+
+        // Séparation des éléments du menu
+        $items_array = explode('</li>', $items);
+
+        // Définition de la position où insérer le lien (après le 2ème élément par exemple)
+        $position = 1; // Modifier cette valeur selon la position souhaitée
+
+        // Insertion du lien Admin à la position désirée
+        array_splice($items_array, $position, 0, $lien_admin);
+
+        // Reconstitution du menu avec le lien Admin ajouté
+        $items = implode('</li>', $items_array);
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'lien_admin');
